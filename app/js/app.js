7b3909c170e476d12ab77ee4672079136967971a
Philip Miglinci <p.miglinci@gmail.com>
2013-03-29 22:13:50 +0100

    prepare for ghpages
    Signed-off-by: Philip Miglinci <p.miglinci@gmail.com>

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,25 @@
 'use strict';
 
+var environment = 'ghpages';
+if (window.location.host.indexOf('localhost') != -1) {
+  environment = 'local';
+}
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui.bootstrap']).
-  config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
-  	$locationProvider.html5Mode(true).hashPrefix('!');
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui.bootstrap']).config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode(false);
+  $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: HomeCtrl});
+  $routeProvider.when('/spiel', {templateUrl: 'partials/spiel.html', controller: SpielCtrl});
+  $routeProvider.when('/ueberuns',{templateUrl: 'partials/ueberuns.html', controller: UeberunsCtrl});
+  $routeProvider.otherwise({redirectTo: '/'});
 
-    $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: HomeCtrl});
-    $routeProvider.when('/spiel', {templateUrl: 'partials/spiel.html', controller: SpielCtrl});
-    $routeProvider.when('/ueberuns',{templateUrl: 'partials/ueberuns.html', controller: UeberunsCtrl});
-    $routeProvider.otherwise({redirectTo: '/'});
+  // if (environment === 'local') {
+  //   $locationProvider.html5Mode(true).hashPrefix('!');
+  //   $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: HomeCtrl});
+  //   $routeProvider.when('/spiel', {templateUrl: 'partials/spiel.html', controller: SpielCtrl});
+  //   $routeProvider.when('/ueberuns',{templateUrl: 'partials/ueberuns.html', controller: UeberunsCtrl});
+  //   $routeProvider.otherwise({redirectTo: '/'});
+  // }
 }]).run(['$rootScope','$location',function($rootScope, $location) {
 	$rootScope.location = $location;
 }]);
